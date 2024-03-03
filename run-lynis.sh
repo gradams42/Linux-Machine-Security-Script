be67450f92e08f1c1293abc119e3c1160c72eb4a
gradams42 <gradams42@tntech.edu>
2024-03-02 21:35:32 -0600

    Update run-lynis.sh

--- run-lynis.sh
+++ run-lynis.sh
@@ -1,2 +1,22 @@
+#!/bin/bash
+
+# Save the current directory path
+current_dir=$(pwd)
+
+# Function to change ownership of Lynis directory to root
+change_ownership() {
+    sudo chown -R root:root "$1"
+}
+
+# Clone Lynis repository
 git clone https://github.com/CISOfy/lynis
-cd lynis && sudo ./lynis audit system
+cd lynis
+
+# Change ownership of Lynis directory to root
+change_ownership "$current_dir/lynis"
+
+# Run Lynis audit with sudo
+sudo ./lynis audit system
+
+# Restore the original directory
+cd "$current_dir"
